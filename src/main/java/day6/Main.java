90732e113657b1667e2cb7b2091f570ef328582d
thomas hugon <thomas_hugon_1@carrefour.com>
2021-12-10 16:58:28 +0100

    day6

--- src/main/java/day6/Main.java
+++ src/main/java/day6/Main.java
@@ -8,7 +8,6 @@ import java.net.URISyntaxException;
 public class Main {
     public static void main(String[] args) throws IOException, URISyntaxException {
         DroolsHelper.execute("/day6/part1_2.drl", "/day6/input.txt");
-
     }
 
 
